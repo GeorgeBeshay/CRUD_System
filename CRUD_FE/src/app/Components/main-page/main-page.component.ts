a1eba106c9dfe82e131993bedff55d3e7bfc6508
Philopater Hany <philopaterkiro@gmail.com>
2023-02-02 18:31:54 +0200

    Sweetalert popups added

--- CRUD_FE/src/app/Components/main-page/main-page.component.ts
+++ CRUD_FE/src/app/Components/main-page/main-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import * as JsBarcode from 'jsbarcode';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -21,6 +21,33 @@ export class MainPageComponent {
       lineColor: 'black',
       margin: 2,
       textMargin: 2,
+    });
+  }
+  addProduct() {
+    document.forms[0].reset();
+    Swal.fire('Added!', 'Your Product has been added successfully.', 'success');
+  }
+  editProduct() {
+    console.log('clicked');
+  }
+  deleteProduct() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your Product has been deleted successfully.',
+          'success'
+        );
+        console.log(this);
+      }
     });
   }
 }
