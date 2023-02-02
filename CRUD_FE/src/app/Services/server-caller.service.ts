a5582ee5623780d289cc0e360b6ae5f883798cd3
George Beshay <90455303+GeorgeBeshay@users.noreply.github.com>
2023-02-03 00:39:57 +0200

    Server Caller Service - Implemented

--- CRUD_FE/src/app/Services/server-caller.service.ts
+++ CRUD_FE/src/app/Services/server-caller.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { firstValueFrom, Observable } from 'rxjs';
+import { Product } from '../Interfaces/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServerCallerService {
-
-  constructor() { }
+  private port = 8081;
+  private url = `http://localhost:${this.port}/server/`;
+  constructor(private http: HttpClient) {}
+  // Function requests to load the products stored in the DB.
+  async load() {
+    return await firstValueFrom(
+      this.http.post<Product[]>(this.url + 'load', null)
+    );
+  }
+  // Function requests to create a product and store it in the DB.
+  async create(product: Product) {
+    return await firstValueFrom(
+      this.http.post<boolean>(this.url + 'create', product)
+    );
+  }
+  // Function requests to update a product and store it in the DB.
+  async update(product: Product) {
+    return await firstValueFrom(
+      this.http.post<boolean>(this.url + 'update', product)
+    );
+  }
+  // Function requests to delete a product and remove it from the DB.
+  async delete(product: Product) {
+    return await firstValueFrom(
+      this.http.post<boolean>(this.url + 'delete', product)
+    );
+  }
+  // Function requests to check for updates.
+  async checkForUpdates() {
+    return await firstValueFrom(
+      this.http.post<boolean>(this.url + 'checkForUpdates', null)
+    );
+  }
+  // Function requests to search among products based on a specific condition
+  async search(condition: number, str: string) {
+    return await firstValueFrom(
+      this.http.post<Product[]>(this.url + `search/${condition}/${str}`, null)
+    );
+  }
 }
