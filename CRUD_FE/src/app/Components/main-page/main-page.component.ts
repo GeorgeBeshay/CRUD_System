f9d63ba076752cfdb708c9534fcfbfa36f2d8fbe
Philopater Hany <philopaterkiro@gmail.com>
2023-02-03 00:28:16 +0200

    Footer & Placeholders

--- CRUD_FE/src/app/Components/main-page/main-page.component.ts
+++ CRUD_FE/src/app/Components/main-page/main-page.component.ts
@@ -7,6 +7,8 @@ import Swal from 'sweetalert2';
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
+  currentYear: any = new Date().getFullYear();
+
   ngOnInit() {
     let data: string = '123456';
     JsBarcode('#barcode', data, {
@@ -23,13 +25,30 @@ export class MainPageComponent {
       textMargin: 2,
     });
   }
+
+  placeHolderFocus(id: any) {
+    document.getElementById(id)?.classList.add('active');
+    document.getElementById(id)?.nextElementSibling?.classList.add('active');
+  }
+
+  placeHolderBlur(id: any) {
+    let input = document.getElementById(id)
+      ?.nextElementSibling as HTMLInputElement;
+    if (input.value === '') {
+      document.getElementById(id)?.classList.remove('active');
+      input.classList.remove('active');
+    }
+  }
+
   addProduct() {
     document.forms[0].reset();
     Swal.fire('Added!', 'Your Product has been added successfully.', 'success');
   }
+
   editProduct() {
     console.log('clicked');
   }
+
   deleteProduct() {
     Swal.fire({
       title: 'Are you sure?',
@@ -37,8 +56,8 @@ export class MainPageComponent {
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
       confirmButtonText: 'Yes, delete it!',
+      cancelButtonColor: '#d33',
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire(
@@ -46,7 +65,6 @@ export class MainPageComponent {
           'Your Product has been deleted successfully.',
           'success'
         );
-        console.log(this);
       }
     });
   }
