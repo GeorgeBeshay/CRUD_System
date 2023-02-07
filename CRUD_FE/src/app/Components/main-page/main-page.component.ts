d7a920b9f78133fc5bf7209e48333b6a2c0b7fe4
Philopater Hany <philopaterkiro@gmail.com>
2023-02-07 02:32:28 +0200

    Edit markdown

--- CRUD_FE/src/app/Components/main-page/main-page.component.ts
+++ CRUD_FE/src/app/Components/main-page/main-page.component.ts
@@ -120,11 +120,6 @@ export class MainPageComponent {
         icon: 'success',
         didClose: () => window.scrollTo(0, 999999),
       });
-      // window.scrollTo({
-      //   top: 99999999,
-      //   left: 0,
-      //   behavior: 'smooth',
-      // });
     } else {
       Swal.fire({
         icon: 'error',
